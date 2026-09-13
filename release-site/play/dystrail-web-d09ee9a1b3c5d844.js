0af24f74af20fbd3263c20cf1415b138adf07447
Vanna DiCatania <vanna@dicatania.me>
2026-09-13 16:50:02 -0700

    chore: record final release build provenance

--- release-site/play/dystrail-web-d09ee9a1b3c5d844.js
+++ release-site/play/dystrail-web-d09ee9a1b3c5d844.js
@@ -248,6 +248,10 @@ function wasm_bindgen__convert__closures________invoke__hf858c3384184f565(arg0, 
     wasm.wasm_bindgen__convert__closures________invoke__hf858c3384184f565(arg0, arg1, arg2);
 }
 
+function wasm_bindgen__convert__closures_____invoke__h5462ef5a5d4b04eb(arg0, arg1) {
+    wasm.wasm_bindgen__convert__closures_____invoke__h5462ef5a5d4b04eb(arg0, arg1);
+}
+
 function wasm_bindgen__convert__closures_____invoke__h210570c76412740a(arg0, arg1, arg2) {
     wasm.wasm_bindgen__convert__closures_____invoke__h210570c76412740a(arg0, arg1, arg2);
 }
@@ -258,10 +262,6 @@ function wasm_bindgen__convert__closures_____invoke__h07de2c698afc4fb1(arg0, arg
 
 function wasm_bindgen__convert__closures________invoke__hd3cf6a8c366cf46b(arg0, arg1, arg2) {
     wasm.wasm_bindgen__convert__closures________invoke__hd3cf6a8c366cf46b(arg0, arg1, arg2);
-}
-
-function wasm_bindgen__convert__closures_____invoke__h5462ef5a5d4b04eb(arg0, arg1) {
-    wasm.wasm_bindgen__convert__closures_____invoke__h5462ef5a5d4b04eb(arg0, arg1);
 }
 
 function wasm_bindgen__convert__closures_____invoke__hce09af24c6ac581e(arg0, arg1, arg2) {
